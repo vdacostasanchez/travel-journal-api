2f55b3f3a3a1217b1a6e5145a90ce1edbda949c3
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-12-20 19:16:42 -0500

    Add model Trip with seeds for initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,3 @@
-
+Trip.create(user_id: 1, start_date: 20001207, end_date: 20231221, location: "Spain")
+Trip.create(user_id: 1, start_date: 20001207, end_date: 20231221, location: "London")
+Trip.create(user_id: 1, start_date: 20001207, end_date: 20231221, location: "Australia")
