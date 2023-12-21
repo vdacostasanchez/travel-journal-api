fb73cc0aef69195f9689a29d7d3a5ec6359cfe1f
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-12-20 21:47:47 -0500

    adds create action for place model:

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,4 +3,15 @@ class PlacesController < ApplicationController
     @places = Place.all
     render :index
   end
+
+  def create
+    @place = Place.create(
+      trip_id: params[:trip_id],
+      user_id: params[:user_id],
+      name: params[:name],
+      date: params[:date],
+      address: params[:address],
+    )
+    render :show
+  end
 end
--- test/controllers/places_controller_test.rb
+++ test/controllers/places_controller_test.rb
@@ -8,4 +8,11 @@ class PlacesControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal Place.count, data.length
   end
+
+  test "create" do
+    assert_difference "Place.count", 1 do
+      post "/places.json", params: { user_id: 1, trip_id: 1, name: "Lake", date: 20231220, address: "Place st. " }
+      assert_response 200
+    end
+  end
 end
