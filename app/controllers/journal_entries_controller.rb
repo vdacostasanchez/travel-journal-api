9eb0143d293ae6977b75a5889719940d74694d47
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-12-20 21:05:02 -0500

    adds show action to journal entry

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -3,4 +3,21 @@ class JournalEntriesController < ApplicationController
     @journal_entries = JournalEntry.all
     render :index
   end
+
+  def create
+    @journal_entry = JournalEntry.create(
+      title: params[:title],
+      date: params[:date],
+      entry: params[:entry],
+      trip_id: params[:trip_id],
+      user_id: params[:user_id],
+      public: params[:public],
+    )
+    render :show
+  end
+
+  def show
+    @journal_entry = JournalEntry.find_by(id: params[:id])
+    render :show
+  end
 end
--- test/controllers/journal_entries_controller_test.rb
+++ test/controllers/journal_entries_controller_test.rb
@@ -8,4 +8,19 @@ class JournalEntriesControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal JournalEntry.count, data.length
   end
+
+  test "create" do
+    assert_difference "JournalEntry.count", 1 do
+      post "/journal_entries.json", params: { title: "Title", date: 20000101, entry: "This is not a really long entry", trip_id: 1, user_id: 1, public: false }
+      assert_response 200
+    end
+  end
+
+  test "show" do
+    get "/journal_entries/#{JournalEntry.first.id}.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal ["id", "title", "date", "entry", "trip_id", "user_id", "public", "created_at", "updated_at"], data.keys
+  end
 end
